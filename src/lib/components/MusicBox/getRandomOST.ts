e803225c87f9a30975ed0d383cece0f00a4a0915
FlamingoLindo <vitorantunes2003@gmail.com>
2026-05-06 16:42:03 -0300

    fix: remove church_ost

--- src/lib/components/MusicBox/getRandomOST.ts
+++ src/lib/components/MusicBox/getRandomOST.ts
@@ -1,4 +1,3 @@
-// FIX change the church_ost to some other one, the start is really loud
 const audioFiles = import.meta.glob('/src/lib/assets/audios/*',
     { eager: true, query: '?url', import: 'default' });
 
